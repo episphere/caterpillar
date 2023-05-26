9dc7633dc722e2afc772c909fd9a5b2940e3f6aa
Jonas Almeida <jalmeida@mathbiol.org>
2023-05-26 16:56:22 -0400

    system segment started

--- caterpillar.js
+++ caterpillar.js
@@ -2,7 +2,9 @@ console.log(`${location.origin}gpt/min.js imported\n${Date()}`)
 
 import * as min from '../gpt/min.js';
 
-function caterpillar(div){
+let msgs = []
+
+function ui(div){
     if(!div){
         // try default div
         div=document.querySelector('#caterpillarDiv')
@@ -11,8 +13,58 @@ function caterpillar(div){
         }
     }
     
-    console.log('caterpillar div')
+    console.log('assembling caterpillar div ...')
+    div.innerHTML=`
+        Model: <select id='model'></select>
+        <br>Temperature: <input id="temperatureNum" size="4" value="0.7"><input id="temperatureRange" type="range" value="0.7" min="0" max="2" step="0.1" size="10">`
+    // select model
+    let model=div.querySelector('#model')
+    min.listModels().then(x=>{
+        x.data.forEach(x=>{
+            let opt = document.createElement('option')
+            opt.value=opt.textContent=x.id
+            model.appendChild(opt)
+        })
+        model.value='gpt-3.5-turbo'
+    })
+    // select temperatureNum
+    let temperatureNum=div.querySelector('#temperatureNum')
+    let temperatureRange=div.querySelector('#temperatureRange')
+    temperatureRange.onchange=function(){
+        temperatureNum.value=temperatureRange.value
+    }
+    temperatureNum.onchange=function(){
+        if(temperatureNum.value>2){temperatureNum.value=2}
+        if(temperatureNum.value<0){temperatureNum.value=0}
+        temperatureRange.value=temperatureNum.value
+    }
+    msgSegment(div)
     return div
 }
 
-export {caterpillar}
+function msgSegment(div, role='system',txt='You are a helpful assistant'){
+    let id=crypto.randomUUID()
+    msgs.push({
+        role:role,
+        content:txt,
+        id:id
+    })
+    let di=document.createElement('div')
+    di.contentEditable=true
+    di.style.marginTop='20px'
+    di.style.border='solid'
+    di.style.borderWidth='1px'
+    di.style.padding='20px'
+    di.style.width=div.clientWidth
+    di.textContent=txt
+    if(role=='system'){
+        di.style.color='maroon'
+    }else{
+        
+    }
+    div.appendChild(di)
+}
+
+ui()
+
+export {ui,msgs}
--- index.js
+++ index.js
@@ -3,25 +3,8 @@ console.log(`${location.href}index.js loaded\n${Date()}`);
 // importing only basic functionality of export.js
 // no UI components included, no auth thrigger
 
-/*
-import { 
-    key,
-    check4key,
-    completions,
-    embeddings,
-    listModels,
-    retrieveModel,
-    models 
-} from "../gpt/export.js";
-
-
-(async function(){
-    lala = await import('../gpt/min.js')
-})()
-*/
-
 (async function(){
     //lala = await import('../gpt/min.js')
-    lala = await import('./caterpillar.js')
+    caterpillar = await import('./caterpillar.js')
     //console.log('hello')
 })();
