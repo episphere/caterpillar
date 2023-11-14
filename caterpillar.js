32b7cebd86956b5df73cdef1502fa69b75651678
Jonas Almeida <jalmeida@mathbiol.org>
2023-11-14 09:50:39 -0500

    Update caterpillar.js

--- caterpillar.js
+++ caterpillar.js
@@ -65,9 +65,6 @@ function addSegmentTo(div, role='system',txt='You are a helpful assistant'){
     div.onresize=function(){
         return Math.min(div.clientWidth,200)
     }
-    div.parentElement.onresize=function(){
-        return Math.min(div.clientWidth,200)
-    }
     if(div.parentElement.onresize){
         div.parentElement.onresize=function(){
             return Math.min(div.clientWidth,200)
